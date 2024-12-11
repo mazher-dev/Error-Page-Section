d36d91d55ad16e61dc01d86a40e9492771ecc656
MH Code <mazheriftikhar1@gmail.com>
2024-12-11 14:07:06 +0500

    11-Dec changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import Error1 from './components/Error1/Error1'
+import Error2 from './components/Error2/Error2'
+import Error3 from './components/Error3/Error3'
+import Error4 from './components/Error4/Error4'
+import Error5 from './components/Error5/Error5'
+import Error6 from './components/Error6/Error6'
+import Error7 from './components/Error7/Error7'
+import Error8 from './components/Error8/Error8'
 
 
 
@@ -7,6 +14,14 @@ const App = () => {
   return (
     <div>
       <Error1 />
+      <Error2 />
+      <Error3 />
+      <Error4 />
+      <Error5 />
+      <Error6 />
+      <Error7 />
+      <Error8 />
+
     </div>
   )
 }
